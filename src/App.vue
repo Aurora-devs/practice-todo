5e2351939916718349d54098ad15fb9538a60e88
Dania99dev <dania99.dev@gmail.com>
2021-05-21 13:49:52 +0430

    boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AddTodo />
+  <main>
+    <h1>Todo</h1>
+    <section class="date">
+      <Date />
+      <WeekDay />
+    </section>
+    <TodoList />
+    <button class="add-btn">+</button>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap");
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Montserrat", sans-serif;
+  background: #f0efe9;
+  color: #555555;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  background-color: white;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.25);
+}
+.add-btn {
+  background-color: #6ee7b7;
+  color: #059669;
+  font-size: 3rem;
+  border: none;
+  height: 4rem;
+  width: 4rem;
+  border-radius: 50%;
+  margin-bottom: -3rem;
 }
 </style>
